f5845bd5943b235d427f4d62c5121786bc1fbfdc
DerEros <dereros@noreply.users.github.com>
2019-12-07 21:10:18 +0100

    Add configuration base class for animations

---
+++ config/AnimationConfig.h
@@ -0,0 +1,13 @@
+#ifndef ANIMATIONCONFIG_H
+#define ANIMATIONCONFIG_H
+
+class AnimationConfig {
+    private:
+
+
+    public:
+        AnimationConfig();
+        virtual ~AnimationConfig();
+};
+
+#endif /* ANIMATIONCONFIG_H */
